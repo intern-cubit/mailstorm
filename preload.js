13a15709e8f3fc5ec69bb21be98dd460f1a17d04
intern-cubit <intern.cubitdynamics@gmail.com>
2025-06-16 13:37:20 +0530

    hello

--- preload.js
+++ preload.js
@@ -4,6 +4,6 @@ contextBridge.exposeInMainWorld('electronAPI', {
     restartApp: () => ipcRenderer.send('restart_app'),
     onUpdateStatus: (callback) => ipcRenderer.on('update-status', (_event, value) => callback(value)),
     onUpdateAvailable: (callback) => ipcRenderer.on('update-available', (_event, version) => callback(version)),
-    onUpdateDownloaded: (callback) => ipcRenderer.on('update-downloaded', (callback)),
+    onUpdateDownloaded: (callback) => ipcRenderer.on('update-downloaded', (_event) => callback()),
     onUpdateProgress: (callback) => ipcRenderer.on('update-progress', (_event, percent) => callback(percent))
 });
